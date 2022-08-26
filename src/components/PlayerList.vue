a72e9402a95009fb1d5b37c99758679c20e46cf2
Blechlawine <marczinser@gmx.de>
2022-08-26 12:50:20 +0200

    show player data in list

--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="flex">
-        <div class="player" v-for="player in PlayerStore.players" :key="player.id">{{ player.name }}</div>
-    </div>
+    <table class="table table-compact w-full">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Card-amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="player in PlayerStore.players" class="hover" :key="player.id">
+                <td>{{ player.name }}</td>
+                <td>{{ player.cardAmount }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 <script setup lang="ts">
 import usePlayers from "../store/playerStore";
