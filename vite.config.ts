dfa3efd735c314b98b91a301e9e45cc266f459ef
Blechlawine <marczinser@gmx.de>
2022-09-21 16:23:50 +0200

    pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })]
-})
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Cluedo-app",
+                short_name: "Cluedoapp",
+                description: "Cluedo-app",
+                icons: [
+                    {
+                        src: "/vite.svg",
+                        sizes: "144x144",
+                    },
+                ],
+            },
+        }),
+    ],
+});
