cc0a49c6880d5a7ce1575f3914a1b5c7dd7368d5
Blechlawine <marczinser@gmx.de>
2023-07-17 17:35:38 +0200

    feat: add static endpoint for serving frontend from server

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,31 +6,36 @@ mod game_id;
 use dotenv::dotenv;
 use game_id::GameId;
 use rocket::data::{Data, ToByteUnit};
+use rocket::fs::FileServer;
 use rocket::serde::json::Json;
 use rocket::tokio::fs::File;
-use rocket::Config;
-use std::path::Path;
+use rocket::{Config, State};
 use std::env;
+use std::path::Path;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+struct AppState {
+    upload_dir: String,
 }
 
+// #[get("/<path..>")]
+// async fn index(state: &State<AppState>, path: PathBuf) -> Option<NamedFile> {
+//     let app_dir = &state.app_dir;
+//     NamedFile::open(Path::new(app_dir).join(path)).await.ok()
+// }
+
 #[post("/save/<id>", data = "<game_data>")]
-async fn save(id: GameId<'_>, game_data: Data<'_>) -> std::io::Result<String> {
+async fn api_save(id: GameId<'_>, game_data: Data<'_>) -> std::io::Result<String> {
     game_data
         .open(128.kibibytes())
         .into_file(id.file_path())
         .await?;
-    Ok(uri!(get_by_id(id)).to_string())
+    Ok(uri!(api_get_by_id(id)).to_string())
 }
 
 #[get("/list")]
-fn list() -> Json<Vec<String>> {
+fn api_list(state: &State<AppState>) -> Json<Vec<String>> {
     // list all files in upload directory
-    let upload_dir = env::var("UPLOAD_DIR").unwrap_or("upload".to_owned());
-    let root = Path::new(&upload_dir);
+    let root = Path::new(&state.upload_dir);
     let files = root
         .read_dir()
         .unwrap()
@@ -41,7 +46,7 @@ fn list() -> Json<Vec<String>> {
 }
 
 #[get("/<id>")]
-async fn get_by_id(id: GameId<'_>) -> Option<File> {
+async fn api_get_by_id(id: GameId<'_>) -> Option<File> {
     File::open(id.file_path()).await.ok()
 }
 
@@ -54,12 +59,19 @@ fn not_found() -> Json<&'static str> {
 fn rocket() -> _ {
     dotenv().ok();
 
-    let port = env::var("PORT").unwrap_or("8000".to_owned()).parse::<_>().unwrap();
+    let port = env::var("PORT")
+        .unwrap_or("8000".to_owned())
+        .parse::<_>()
+        .unwrap();
+    let app_dir = env::var("APP_DIR").unwrap_or("/srv/app".to_owned());
+    let upload_dir = env::var("UPLOAD_DIR").unwrap_or("/srv/upload".to_owned());
     rocket::build()
+        .manage(AppState { upload_dir })
         .configure(Config {
-            port: port,
+            port,
             ..Default::default()
         })
-        .mount("/", routes![index, save, get_by_id, list])
+        .mount("/", FileServer::from(app_dir))
+        .mount("/api", routes![api_save, api_list, api_get_by_id])
         .register("/", catchers![not_found])
 }
