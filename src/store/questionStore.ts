796d89a0a629a59289d1d9fc75cc86bde504919d
Blechlawine <marczinser@gmx.de>
2024-05-19 22:45:14 +0200

    feat: automatically set cards of players that didn't have any of a question to false

--- src/store/questionStore.ts
+++ src/store/questionStore.ts
@@ -1,11 +1,14 @@
 import { defineStore } from "pinia";
 import { type QuestionInput, type QuestionOutput, QuestionValidator } from "../types/validators";
 import { ref } from "vue";
+import usePlayerCardRelations from "./playerCardRelationStore";
 
 const useQuestions = defineStore(
     "questions",
     () => {
         const questions = ref<QuestionOutput[]>([]);
+
+        const PlayerCardRelationStore = usePlayerCardRelations();
 
         function getByID(id: string): QuestionOutput | undefined {
             return questions.value.find((q) => q.id === id);
@@ -20,6 +23,26 @@ const useQuestions = defineStore(
                     Object.assign(temp, data);
                 } else {
                     questions.value.push(data);
+                }
+                // automatically set the asked cards for all players that didn't have any to false
+                if (data.playersThatDidntHaveAnythingIds.length > 0) {
+                    for (const playerId of data.playersThatDidntHaveAnythingIds) {
+                        PlayerCardRelationStore.upsert({
+                            playerId,
+                            cardId: data.suspectCardId,
+                            value: false,
+                        });
+                        PlayerCardRelationStore.upsert({
+                            playerId,
+                            cardId: data.weaponCardId,
+                            value: false,
+                        });
+                        PlayerCardRelationStore.upsert({
+                            playerId,
+                            cardId: data.locationCardId,
+                            value: false,
+                        });
+                    }
                 }
             }
         }
