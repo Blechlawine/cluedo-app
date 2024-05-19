2e308e463c362d5e10e56971069538a621d71831
Blechlawine <marczinser@gmx.de>
2024-05-19 22:34:28 +0200

    chore: refactor remaining stores to composition api

--- src/store/playerCardRelationStore.ts
+++ src/store/playerCardRelationStore.ts
@@ -1,52 +1,52 @@
 import { defineStore } from "pinia";
 import {
-    PlayerCardRelationInput,
-    PlayerCardRelationOutput,
+    type PlayerCardRelationInput,
+    type PlayerCardRelationOutput,
     PlayerCardRelationValidator,
 } from "../types/validators";
-interface IState {
-    playerCardRelations: PlayerCardRelationOutput[];
-}
-type TGetters = {
-    getByPlayerIdAndCardId: (
-        state: IState,
-    ) => (playerId: string, cardId: string) => PlayerCardRelationOutput | undefined;
-    getByCardId: (state: IState) => (cardId: string) => PlayerCardRelationOutput[] | [];
-};
-interface IActions {
-    upsert: (PlayerCardRelation: PlayerCardRelationInput) => void;
-}
-const usePlayerCardRelations = defineStore<"playerCardRelations", IState, TGetters, IActions>(
+import { ref } from "vue";
+
+const usePlayerCardRelations = defineStore(
     "playerCardRelations",
-    {
-        state: () => ({
-            playerCardRelations: [],
-        }),
-        persist: true,
-        getters: {
-            getByPlayerIdAndCardId: (state) => (playerId, cardId) =>
-                state.playerCardRelations.find(
-                    (p) => p.playerId === playerId && p.cardId === cardId,
-                ),
-            getByCardId: (state) => (cardId) =>
-                state.playerCardRelations.filter((p) => p.cardId === cardId),
-        },
-        actions: {
-            upsert(playerCardRelation) {
-                const parsed = PlayerCardRelationValidator.safeParse(playerCardRelation);
-                if (parsed.success) {
-                    const data = parsed.data;
-                    const temp = this.playerCardRelations.find(
-                        (c) => c.playerId === data.playerId && c.cardId === data.cardId,
-                    );
-                    if (temp) {
-                        Object.assign(temp, data);
-                    } else {
-                        this.playerCardRelations.push(data);
-                    }
+    () => {
+        const playerCardRelations = ref<PlayerCardRelationOutput[]>([]);
+
+        function getByPlayerIdAndCardId(
+            playerId: string,
+            cardId: string,
+        ): PlayerCardRelationOutput | undefined {
+            return playerCardRelations.value.find(
+                (p) => p.playerId === playerId && p.cardId === cardId,
+            );
+        }
+
+        function getByCardId(cardId: string): PlayerCardRelationOutput[] {
+            return playerCardRelations.value.filter((p) => p.cardId === cardId);
+        }
+
+        function upsert(playerCardRelation: PlayerCardRelationInput) {
+            const parsed = PlayerCardRelationValidator.safeParse(playerCardRelation);
+            if (parsed.success) {
+                const data = parsed.data;
+                const temp = playerCardRelations.value.find(
+                    (c) => c.playerId === data.playerId && c.cardId === data.cardId,
+                );
+                if (temp) {
+                    Object.assign(temp, data);
+                } else {
+                    playerCardRelations.value.push(data);
                 }
-            },
-        },
+            }
+        }
+
+        return {
+            playerCardRelations,
+            getByPlayerIdAndCardId,
+            getByCardId,
+            upsert,
+        };
     },
+    { persist: true },
 );
+
 export default usePlayerCardRelations;
--- src/store/questionStore.ts
+++ src/store/questionStore.ts
@@ -1,39 +1,42 @@
 import { defineStore } from "pinia";
-import { QuestionInput, QuestionOutput, QuestionValidator } from "../types/validators";
-interface IState {
-    questions: QuestionOutput[];
-}
-type TGetters = {
-    getByID: (state: IState) => (id: string) => QuestionOutput | undefined;
-};
-interface IActions {
-    upsert: (question: QuestionInput) => void;
-    deleteByID: (id: string) => void;
-}
-const useQuestions = defineStore<"questions", IState, TGetters, IActions>("questions", {
-    state: () => ({
-        questions: [],
-    }),
-    persist: true,
-    getters: {
-        getByID: (state) => (id) => state.questions.find((q) => q.id === id),
-    },
-    actions: {
-        upsert(question) {
+import { type QuestionInput, type QuestionOutput, QuestionValidator } from "../types/validators";
+import { ref } from "vue";
+
+const useQuestions = defineStore(
+    "questions",
+    () => {
+        const questions = ref<QuestionOutput[]>([]);
+
+        function getByID(id: string): QuestionOutput | undefined {
+            return questions.value.find((q) => q.id === id);
+        }
+
+        function upsert(question: QuestionInput) {
             const parsed = QuestionValidator.safeParse(question);
             if (parsed.success) {
                 const data = parsed.data;
-                let temp = this.questions.find((q) => q.id === data.id);
+                const temp = questions.value.find((q) => q.id === data.id);
                 if (temp) {
                     Object.assign(temp, data);
                 } else {
-                    this.questions.push(data);
+                    questions.value.push(data);
                 }
             }
-        },
-        deleteByID(id) {
-            this.questions = this.questions.filter((q) => q.id !== id);
-        },
+        }
+
+        function deleteByID(id: string) {
+            questions.value = questions.value.filter((q) => q.id !== id);
+        }
+
+        return {
+            questions,
+            getByID,
+            upsert,
+            deleteByID,
+        };
     },
-});
+    {
+        persist: true,
+    },
+);
 export default useQuestions;
