476aab811d91b24ae0a8d0cbfd79e96c31f88b80
Blechlawine <marczinser@gmx.de>
2024-10-09 19:38:32 +0200

    feat: add super cluedo cards and custom avengers cards

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -8,31 +8,21 @@
                 <th class="w-min">{{ $t("delete") }}</th>
             </tr>
         </thead>
-        <Draggable
-            tag="tbody"
-            :list="CardStore.cards"
-            handle=".drag-handle"
-            ghost-class="opacity-50"
-        >
+        <Draggable tag="tbody" :list="CardStore.cards" handle=".drag-handle" ghost-class="opacity-50">
             <tr v-for="card in CardStore.cards" :key="card.id" class="hover">
                 <td class="drag-handle">
                     <div class="i-tabler-grip-vertical"></div>
                 </td>
                 <td class="whitespace-nowrap text-ellipsis overflow-hidden">{{ card.name }}</td>
                 <td>
-                    <label
-                        :for="props.label"
-                        class="btn btn-sm btn-square btn-ghost"
-                        @click="() => onEditClick(card.id)"
-                    >
+                    <label :for="props.label" class="btn btn-sm btn-square btn-ghost"
+                        @click="() => onEditClick(card.id)">
                         <div class="i-tabler-pencil"></div>
                     </label>
                 </td>
                 <td>
-                    <button
-                        class="btn btn-sm btn-square btn-ghost hover:btn-error"
-                        @click="() => onDeleteClick(card.id)"
-                    >
+                    <button class="btn btn-sm btn-square btn-ghost hover:btn-error"
+                        @click="() => onDeleteClick(card.id)">
                         <div class="i-tabler-trash"></div>
                     </button>
                 </td>
@@ -40,13 +30,17 @@
         </Draggable>
     </table>
     <div class="p-2 flex flex-row gap-2 items-center justify-center" v-if="!CardStore.cards.length">
-        <!-- <button @click="addSuperCluedoCards" class="btn btn-sm">
+        <button @click="addSuperCluedoCards" class="btn btn-sm">
             <div class="i-tabler-plus"></div>
             Super Cluedo
-        </button> -->
+        </button>
         <button @click="addCluedoCards" class="btn btn-sm">
             <div class="i-tabler-plus"></div>
             Cluedo
+        </button>
+        <button @click="addAvengersCards" class="btn btn-sm">
+            <div class="i-tabler-plus"></div>
+            Avengers
         </button>
     </div>
 </template>
@@ -76,7 +70,42 @@ const onDeleteClick = (id: string) => {
     CardStore.deleteByID(id);
 };
 
-function addSuperCluedoCards() {}
+function addSuperCluedoCards() {
+    const cards = [
+        { name: t("cardName.suspect.purple"), category: "suspect" },
+        { name: t("cardName.suspect.brown"), category: "suspect" },
+        { name: t("cardName.suspect.yellow"), category: "suspect" },
+        { name: t("cardName.suspect.red"), category: "suspect" },
+        { name: t("cardName.suspect.green"), category: "suspect" },
+        { name: t("cardName.suspect.grey"), category: "suspect" },
+        { name: t("cardName.suspect.orange"), category: "suspect" },
+        { name: t("cardName.suspect.blue"), category: "suspect" },
+        { name: t("cardName.suspect.pink"), category: "suspect" },
+        { name: t("cardName.suspect.white"), category: "suspect" },
+        { name: t("cardName.weapon.dagger"), category: "weapon" },
+        { name: t("cardName.weapon.poison"), category: "weapon" },
+        { name: t("cardName.weapon.pipe"), category: "weapon" },
+        { name: t("cardName.weapon.horseshoe"), category: "weapon" },
+        { name: t("cardName.weapon.light"), category: "weapon" },
+        { name: t("cardName.weapon.revolver"), category: "weapon" },
+        { name: t("cardName.weapon.spanner"), category: "weapon" },
+        { name: t("cardName.weapon.rope"), category: "weapon" },
+        { name: t("cardName.location.study"), category: "location" },
+        { name: t("cardName.location.library"), category: "location" },
+        { name: t("cardName.location.billiardroom"), category: "location" },
+        { name: t("cardName.location.garden"), category: "location" },
+        { name: t("cardName.location.kitchen"), category: "location" },
+        { name: t("cardName.location.lounge"), category: "location" },
+        { name: t("cardName.location.pavillion"), category: "location" },
+        { name: t("cardName.location.salon"), category: "location" },
+        { name: t("cardName.location.diningroom"), category: "location" },
+        { name: t("cardName.location.fountain"), category: "location" },
+        { name: t("cardName.location.stables"), category: "location" },
+        { name: t("cardName.location.wintergarden"), category: "location" },
+    ] as CardInput[];
+    CardStore.insertMany(cards);
+}
+
 function addCluedoCards() {
     const cards = [
         { name: t("cardName.suspect.red"), category: "suspect" },
@@ -92,7 +121,7 @@ function addCluedoCards() {
         { name: t("cardName.weapon.light"), category: "weapon" },
         { name: t("cardName.weapon.rope"), category: "weapon" },
         { name: t("cardName.location.hall"), category: "location" },
-        { name: t("cardName.location.lounge"), category: "location" },
+        { name: t("cardName.location.salon"), category: "location" },
         { name: t("cardName.location.diningroom"), category: "location" },
         { name: t("cardName.location.kitchen"), category: "location" },
         { name: t("cardName.location.conservatory"), category: "location" },
@@ -100,6 +129,54 @@ function addCluedoCards() {
         { name: t("cardName.location.billiardroom"), category: "location" },
         { name: t("cardName.location.library"), category: "location" },
         { name: t("cardName.location.study"), category: "location" },
+    ] as CardInput[];
+    CardStore.insertMany(cards);
+}
+
+function addAvengersCards() {
+    const cards = [
+        { name: t("cardName.suspect.avengers.captain_america"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.doctor_strange"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.wasp"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.yondu"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.captain_marvel"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.hulk"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.spiderman"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.pepper"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.thor"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.starlord"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.hawkeye"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.black_panther"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.iron_man"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.gamora"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.black_widow"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.groot"), category: "suspect" },
+        { name: t("cardName.suspect.avengers.rocket"), category: "suspect" },
+        { name: t("cardName.weapon.avengers.sceptre"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.knife"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.revolver"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.powerstone"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.slingring"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.tesseract"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.shink-disc"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.thors_hammer"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.shield"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.web_shooter"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.gauntlet"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.blaster"), category: "weapon" },
+        { name: t("cardName.weapon.avengers.bow_and_arrow"), category: "weapon" },
+        { name: t("cardName.location.study"), category: "location" },
+        { name: t("cardName.location.library"), category: "location" },
+        { name: t("cardName.location.billiardroom"), category: "location" },
+        { name: t("cardName.location.garden"), category: "location" },
+        { name: t("cardName.location.kitchen"), category: "location" },
+        { name: t("cardName.location.lounge"), category: "location" },
+        { name: t("cardName.location.pavillion"), category: "location" },
+        { name: t("cardName.location.salon"), category: "location" },
+        { name: t("cardName.location.diningroom"), category: "location" },
+        { name: t("cardName.location.fountain"), category: "location" },
+        { name: t("cardName.location.stables"), category: "location" },
+        { name: t("cardName.location.wintergarden"), category: "location" },
     ] as CardInput[];
     CardStore.insertMany(cards);
 }
